1906cf0cd5e567b609a38e70f2a76a96777f1e82
frankysaxena <frankysaxena@prolificacademic.co.uk>
2015-08-05 17:38:38 +0100

    Successful feature extraction
    Can extract features such as: numbers of fb friends and ip addresses, firstname,
    ethnicity, browsers, devices, browser_version, browser_lang, ip_org, current_
    country_of_residence

--- getting_features.py
+++ getting_features.py
@@ -19,11 +19,11 @@ user = db["user"]
 # digital_finger_print = db["digital_finger_print"]
 ip_info = db["ip_info"]
 
-x = db.user.find({"is_facebook_email_verified_stored" : True }).count()
+#x = db.user.find({"is_facebook_email_verified_stored" : True }).count()
 
-a = randint(0,x)
+#a = randint(0,x)
 
-A = db.user.find_one({"is_facebook_email_verified_stored" : True })
+A = db.user.find_one({"is_banned" : True })
 #B = db.user.find({"is_banned" : False }).limit(-1).skip(a).next()
 
 # db.digital_finger_print.find().limit(-1).skip(a).next()
@@ -70,6 +70,11 @@ def get_devices(user):
 
 def get_browsers(user):
 	return get_digital_finger_print_values_for_user(user, "browser")
+#	N = set(name)
+#	D = dict( zip(N, range(len(N))) )
+#	Y = [D[name_] for name_ in name]
+#	return Y
+
 
 def get_browser_versions(user):
 	return get_digital_finger_print_values_for_user(user, "browser_version")
@@ -80,6 +85,11 @@ def get_browser_lang(user):
 def get_ips_for_user(user):
 	ip_infos = get_ip_infos_for_user(user)
 	return [ip_info.get('ip','') for ip_info in ip_infos]
+
+def get_num_ips_for_user(user):
+	ip_infos = get_ip_infos_for_user(user)
+	num_ips = len(ip_infos)
+	return num_ips
 
 def get_ips_orgs_for_user(user):
 	ip_infos = get_ip_infos_for_user(user)
@@ -96,15 +106,33 @@ def get_feature_dict_for_user(user):
 	feature_dict['firstname'] = user.get("firstname")
 	feature_dict['ethnicity'] = user.get("_ethnicity")
 	feature_dict['browsers'] = get_browsers(user)
+	feature_dict['devices'] = get_devices(user)
 	feature_dict['browser_version'] = get_browser_versions(user)
 	feature_dict['browser_lang'] = get_browser_lang(user)
-	feature_dict['ip'] = get_ips_for_user(user)
+	feature_dict['num_ip'] = get_num_ips_for_user(user)
 	feature_dict['ip_org'] = get_ips_orgs_for_user(user)
 	feature_dict['_current_country_of_residence'] = user.get("_current_country_of_residence")
 	try:
-		feature_dict['device'] = get_devices(user)[0]
+		feature_dict['devices'] = get_devices(user)[0]
 	except IndexError:
-		feature_dict['device'] = "UNKNOWN"
+		feature_dict['devices'] = "UNKNOWN"
+	try:
+		feature_dict['browsers'] = get_browsers(user)[0]
+	except IndexError:
+		feature_dict['browsers'] = "UNKNOWN"
+	try:
+		feature_dict['browser_lang'] = get_browser_lang(user)[0]
+	except IndexError:
+		feature_dict['browser_lang'] = "UNKNOWN"
+	try:
+		feature_dict['ip_org'] = get_ips_orgs_for_user(user)[0]
+	except IndexError:
+		feature_dict['ip_org'] = "UNKNOWN"
+	try:
+		feature_dict['browser_version'] = get_browser_versions(user)[0]
+	except IndexError:
+		feature_dict['browser_version'] = "UNKNOWN"
+
 
 	return feature_dict
 
@@ -118,7 +146,16 @@ def get_list_of_feature_dicts(users):
 	return feature_dicts
 
 
-users = [u for u in db.user.find({})]
+users = [u for u in db.user.find({"is_banned" : False})] #Only banned users
 random.shuffle(users)
 
-print get_list_of_feature_dicts(users[:10])
+#print get_list_of_feature_dicts(users[:10])
+
+vec = DictVectorizer()
+
+sample =   get_list_of_feature_dicts(users[:10])
+
+print vec.fit_transform(sample).toarray()
+
+print vec.get_feature_names()
+
